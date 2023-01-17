523890c3c3d8b21c39d47eb31c8d02fbb95617a9
DanielCornejoStona <104890029+DanielCornejoStona@users.noreply.github.com>
2023-01-17 21:15:42 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,19 +3,16 @@ let arraycasi = [];
 
 
 class Jugador{
-    constructor(id,numFichas){
-        this.id = id;
+    constructor(id,fichasganadas,numFichas){
+        this.id=id;
+        this.fichasganadas = fichasganadas;
         this.numFichas = numFichas;
-    }
-
-    getId(){
-        return this.id;
     }
 
      crearJugadores(a){
         for(var i = 1; i<=a; i++){
             let fichas = Math.floor(50/a);
-            let jugador = new Jugador(i,fichas);
+            let jugador = new Jugador(i,0,fichas);
             arrayjug.push(jugador);
         }
     }
@@ -38,7 +35,7 @@ const tres = new Casilla(0,3);
 const cuatro = new Casilla(0,4);
 const cinco = new Casilla(0,5);
 const seis = new Casilla(0,6);
-const siete = new Casilla(0,4);
+const siete = new Casilla(0,300);
 const ocho = new Casilla(0,8);
 const nueve = new Casilla(0,9);
 const diez = new Casilla(0,10);
@@ -47,88 +44,147 @@ arraycasi.push(dos,tres,cuatro,cinco,seis,siete,ocho,nueve,diez,once);
 
 const jug = new Jugador(0,0);
 let num = 0;
+let rondaFinal= false;
 function jugar(){
     if(num==0){
-     num = prompt("¿Cuantos jugadores van a jugar?");
-    jug.crearJugadores(num);
+        do{
+            num = prompt("¿Cuantos jugadores van a jugar?");
+        }while(num<2||num>5);
+        jug.crearJugadores(num);
     }
     cambiarTurno(num);
+    comprobarFichas();
     tirarDados();
     
 }
 
-function victoria(){
-    return false;
+function rondafinal(){
+    if(rondaFinal==false){
+    console.log("Ya no te quedan fichas, tirar dado y recoger fichas hasta vaciar tablero");
+    rondaFinal=true;
+    }
 }
 
-let turno = 1;
+let turno = -1;
 
 function cambiarTurno(b){
-    
-    console.log("Turno del jugador " + turno);
-    console.log(arrayjug[turno - 1]);
     turno++;
-    if(turno>b){
-        turno=1;
+    if(turno>=b){
+        turno=0;
     }
-    tirarDados();
+    let contador = 0;
+    for(var i = 0; i<arraycasi.length; i++){ 
+        contador += arraycasi[i].numFichas;
+    }
+    if(contador==0&&arrayjug[turno].numFichas==0){
+        fin();
+        return false;
+    }
+    console.log("Tablero");
+    for(let i=0; i<arraycasi.length; i++){
+        console.log("Casilla "+ (i+2) + " : " + arraycasi[i].numFichas);
+    }
+    console.log("Turno del jugador " + (turno +1));
+    console.log(arrayjug[turno]);
+
 }
 
 
 
 function tirarDados(){
-    let dado1 = Math.round(Math.random()* (5)+1);
-    let dado2 = Math.round(Math.random() * (5)+1);
-    let suma = dado1+dado2;
-    console.log("Has sacado " + dado1 + " y " + dado2 + " que suman " + suma);
-    comprobarCasilla(suma);
+    if(final==false){
+        let dado1 = Math.round(Math.random()* (5)+1);
+        let dado2 = Math.round(Math.random() * (5)+1);
+        let suma = dado1+dado2;
+        console.log("Has sacado " + dado1 + " y " + dado2 + " que suman " + suma);
+        comprobarCasilla(suma);
+    }
 }
 
 function restarFicha(){
-            arrayjug[turno-1].numFichas-=1;
-            console.log("Se te ha restado una ficha para introducirla en la casilla");
-            jugar();
+        arrayjug[turno].numFichas-=1;
+        console.log("Se te ha restado una ficha para introducirla en la casilla");
+}
+
+function comprobarFichas(){
+    if(arrayjug[turno].numFichas==0){
+        rondafinal();
+        return false;
+    }else{
+        return true;
+    }
 }
 
 function comprobarCasilla(c){
-    if(c==12){
-        if(arrayjug[turno-1].numFichas=!0){
-        arrayjug[turno-1].numFichas+=arraycasi[5].numFichas;
-        arraycasi[5].numFichas=0;
-        }else{ 
+    if(rondaFinal==true){
+        if(c==12){
             let cont = 0;
-            for(var i = 0; i<arraycasi.length; i++){
-                
+            for(var i = 0; i<arraycasi.length; i++){ 
                 cont += arraycasi[i].numFichas;
                 arraycasi[i].numFichas=0;
             }
-            arrayjug[turno-1].numFichas + cont;
-        }
-    }
-    for(var i = 0; i<arraycasi.length; i++){
-        if(c==7){
-            arraycasi[5].numFichas++;
-            if(arraycasi[5].numFichas==4){
-                console.log("Has completado la casilla " + c + " ! Te llevas las " + 4 + " fichas!");
-                    arraycasi[5].numFichas=0;
-                    arrayjug[turno-1].numFichas+=arraycasi[5].maxFichas;
-                    restarFicha();
-            }else{
-                restarFicha();
+            console.log("Te has llevado todas las fichas");
+            arrayjug[turno].fichasganadas += cont;
+            fin();
+            
+        }else{
+            if(final==false){
+                console.log("Te has llevado las fichas de la casilla " + c);
+                arrayjug[turno].fichasganadas+=arraycasi[c-2].numFichas;
+                arraycasi[c-2].numFichas=0;
+                jugar();
             }
         }
-        if(arraycasi[i].maxFichas==c){
-            arraycasi[i].numFichas++;
-                if(arraycasi[i].numFichas==arraycasi[i].maxFichas){
-                    console.log("Has completado la casilla " + c + " ! Te llevas las " + c + " fichas!");
-                    arraycasi[i].numFichas=0;
-                    arrayjug[turno-1].numFichas+=arraycasi[i].maxFichas;
-                    restarFicha();
-            }   else{
+        
+    }else{ 
+        if(c==12){
+            arrayjug[turno].fichasganadas+=arraycasi[5].numFichas;
+            arraycasi[5].numFichas=0;
+            console.log("Te has llevado las fichas del puchero");
+            jugar();
+        }else{
+        arraycasi[c-2].numFichas++;
+        }
+        for(let i = 0; i<arraycasi.length; i++){
+            if(c==7&&final==false){
                 restarFicha();
+                
+                jugar();
             }
+            if(arraycasi[i].maxFichas==c&&final==false){
+                    if(arraycasi[i].numFichas==arraycasi[i].maxFichas){
+                        restarFicha();
+                        console.log("Has completado la casilla " + c + " ! Te llevas las " + c + " fichas!");
+                        arrayjug[turno].fichasganadas+=arraycasi[i].maxFichas;
+                        arraycasi[i].numFichas=0;
+                        jugar();
+                        return false;
+
+                }   else{
+                    restarFicha();
+                    jugar();
+                    return false;
+                }
+            }
+            
         }
     }
 }
+let ganador=0;
+let final=false;
+function fin(){
+    let fich = 0;
+    for(var i=0;i<arrayjug.length;i++){
+        if(arrayjug[i].fichasganadas>fich){
+            fich=arrayjug[i].fichasganadas;
+            ganador=i;
+        }
+    }
+    console.log("Ha ganado el jugador " + arrayjug[ganador].id + " con " + arrayjug[ganador].fichasganadas + " fichas");
+    final=true;
+    return false;
+}
 
+if(final==false){
 jugar();
+}
